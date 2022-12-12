dd142b1214b73fdc59f939c809e596f4aeee3b66
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-12 22:30:23 +0530

    add header component & home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Topbar from "./components/topBar/Topbar";
+import Home from './pages/home/Home'
 
 function App() {
   return (
     <>
       <Topbar />
+      <Home />
     </>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import Header from '../../components/header/Header'
+import './Home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Header />
+        Homepage
+    </div>
+  )
+}
+
+export default Home
