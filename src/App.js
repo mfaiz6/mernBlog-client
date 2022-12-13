02b4c1d4858666730955438e0b01d79ffbc71ba4
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-13 20:33:16 +0530

    add write page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import Single from "./components/single/Single";
 import Topbar from "./components/topBar/Topbar";
 import Home from './pages/home/Home'
+import Write from "./pages/write/Write";
 
 function App() {
   return (
     <>
       <Topbar />
-      <Single />
+      <Write />
     </>
   );
 }
