32bab5a0b61b158d3431b141cefc7d69ae3a5d93
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-14 00:01:55 +0530

    add login and register page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Single from "./components/single/Single";
 import Topbar from "./components/topBar/Topbar";
 import Home from './pages/home/Home'
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Write from "./pages/write/Write";
 
@@ -8,7 +10,7 @@ function App() {
   return (
     <>
       <Topbar />
-      <Settings />
+      <Register />
     </>
   );
 }
