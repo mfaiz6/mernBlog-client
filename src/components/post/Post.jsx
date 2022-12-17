5d2652d566f365be4fec96f50c35d6261804369f
Mohammad Faiz Ahanger <Mohammadfaiz_2019bcse002@nitsri.net>
2022-12-17 13:31:37 +0530

    fetch & display single post

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,20 +1,22 @@
 import './Post.css'
-
-const Post = ({post}) => {
+import {Link} from 'react-router-dom'
+const Post = ({ post }) => {
   return (
     <div className='post'>
-        {post.photo && <img className='postImg' src={post.photo } alt="" />}
-        <div className="postInfo">
-            <div className="postCats">
-              {post.categories.map((c) => (
-                <span className="postCat">{c.name}</span>
-              ))}
-                
-            </div><span className="postTitle">{post.title}</span>
-            <hr />
-            <span className="postDate">{new Date(post.createdAt).toString()}</span>
+      {post.photo && <img className='postImg' src={post.photo} alt="" />}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCat">{c.name}</span>
+          ))}
         </div>
-        <p className='postDesc'>{post.desc}</p>
+        <Link to={`/post/${post._id}`} className="link">
+        <span className="postTitle">{post.title}</span>
+        </Link>
+        <hr />
+        <span className="postDate">{new Date(post.createdAt).toString()}</span>
+      </div>
+      <p className='postDesc'>{post.desc}</p>
     </div>
   )
 }
